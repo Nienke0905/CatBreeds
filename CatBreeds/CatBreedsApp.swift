06ba9d20255db0427c84d917305ea028ffe0ed92
Nienke0905 <nienkewestdijk@gmail.com>
2023-07-19 16:20:56 +0200

    Setup project

---
+++ CatBreeds/CatBreedsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatBreedsApp.swift
+//  CatBreeds
+//
+//  Created by Nienke Westdijk on 19.07.23.
+//
+
+import SwiftUI
+
+@main
+struct CatBreedsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
